57d31654791dfe22ef031a96a9aad4095f7ba6b0
supermeap123 <garrentvanmeter@gmail.com>
2024-11-17 02:52:53 -0800

    mew

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,16 @@
 import sys
 import os
+import pytest
+from unittest.mock import AsyncMock
+import discord
+from discord.ext import commands
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+@pytest.fixture
+def bot():
+    intents = discord.Intents.all()
+    bot = commands.Bot(command_prefix="!", intents=intents)
+    bot.api_client = AsyncMock() # Add api_client attribute
+    bot.add_cog(AsyncMock())
+    return bot
